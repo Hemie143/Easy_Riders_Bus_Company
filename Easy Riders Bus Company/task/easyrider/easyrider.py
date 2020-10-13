210bb193a2b45240eb111486b848bdf90fd8e78d
hemie143 <hemie143@gmail.com>
2020-10-13 21:09:27 +0200

    Stage 5 Ongoing

--- Easy Riders Bus Company/task/easyrider/easyrider.py
+++ Easy Riders Bus Company/task/easyrider/easyrider.py
@@ -1,6 +1,6 @@
 import json
 import re
-# 15 min
+from datetime import datetime
 
 data = json.loads(input())
 
@@ -39,6 +39,7 @@ def check_valid(data):
     for k, v in errors.items():
         print(f'{k}: {v}')
 
+
 def find_lines(data):
     bus_lines = {}
     for entry in data:
@@ -51,4 +52,110 @@ def find_lines(data):
     for bus_line, stops in bus_lines.items():
         print(f'bus_id: {bus_line}, stops: {len(stops)}')
 
-find_lines(data)
+
+def find_stops(data):
+    bus_starts = {}
+    bus_stops = {}
+    bus_finishes = {}
+    bus_all_stops = {}
+    bus_lines = set()
+    for entry in data:
+        bus_id = entry['bus_id']
+        bus_lines.add(bus_id)
+        stop_type = entry['stop_type']
+        if bus_id not in bus_stops:
+            bus_stops[bus_id] = set()
+        if bus_id not in bus_all_stops:
+            bus_all_stops[bus_id] = set()
+        bus_all_stops[bus_id].add(entry['stop_name'])
+        if stop_type == 'S':
+            if bus_id not in bus_starts:
+                bus_starts[bus_id] = set()
+            bus_starts[bus_id].add(entry['stop_name'])
+        elif stop_type == 'F':
+            if bus_id not in bus_finishes:
+                bus_finishes[bus_id] = set()
+            bus_finishes[bus_id].add(entry['stop_name'])
+        else:
+            bus_stops[bus_id].add(entry['stop_name'])
+
+    all_starts = set()
+    all_finishes = set()
+    all_transfers = set()
+    bus_lines = sorted(bus_lines)
+
+    for i, line in enumerate(bus_lines):
+        if line not in bus_starts or len(bus_starts[line]) != 1:
+            print(f'There is no start for the line: {line}')
+            return
+        if line not in bus_finishes or len(bus_finishes[line]) != 1:
+            print(f'There is no finish for the line: {line}')
+            return
+        all_starts |= bus_starts[line]
+        all_finishes |= bus_finishes[line]
+        for j in range(i + 1, len(bus_lines)):
+            all_transfers |= bus_all_stops[line] & bus_all_stops[bus_lines[j]]
+
+    print(f'Start stops: {len(all_starts)} {sorted(all_starts)}')
+    print(f'Transfer stops: {len(all_transfers)} {sorted(all_transfers)}')
+    print(f'Finish stops: {len(all_finishes)} {sorted(all_finishes)}')
+
+    return
+
+
+def get_bus_stop(data, bus_id, stop_type=None, stop_id=None):
+    for entry in data:
+        if entry['bus_id'] == bus_id:
+            if stop_type and entry['stop_type'] == stop_type:
+                return entry
+            if stop_id and entry['stop_id'] == stop_id:
+                return entry
+    return None
+
+def check_time(data):
+    bus_lines = list(set([e['bus_id'] for e in data]))
+    bus_lines.sort()
+    # print(bus_lines)
+    all_times_ok = True
+    for line in bus_lines:
+        bus_stop = get_bus_stop(data, line, stop_type="S")
+        stop_time = datetime.strptime(bus_stop['a_time'], '%H:%M')
+        next_stop = bus_stop['next_stop']
+        while bus_stop['stop_type'] != "F":
+            bus_stop = get_bus_stop(data, line, stop_id=next_stop)
+            this_time = datetime.strptime(bus_stop['a_time'], '%H:%M')
+            next_stop = bus_stop['next_stop']
+            if this_time <= stop_time:
+                print(f'bus_id line {line}: wrong time on station {bus_stop["stop_name"]}')
+                all_times_ok = False
+                break
+    if all_times_ok:
+        print('OK')
+    return
+
+    '''
+    bus_time = datetime.strptime('00:00', '%H:%M')
+    current_line = None
+    skip_line = None
+    time_errors = False
+    print('Arrival time test:')
+    for entry in data:
+        bus_id = entry['bus_id']
+        if bus_id == skip_line:
+            continue
+        if bus_id != current_line:
+            bus_time = datetime.strptime('00:00', '%H:%M')
+            current_line = bus_id
+        if datetime.strptime(entry['a_time'], '%H:%M') > bus_time or entry['stop_type'] == 'F':
+            bus_time = datetime.strptime(entry['a_time'], '%H:%M')
+        else:
+            print(f'bus_id line {bus_id}: wrong time on station {entry["stop_name"]}')
+            skip_line = bus_id
+            time_errors = True
+    if not time_errors:
+        print('OK')
+    '''
+
+# find_lines(data)
+# find_stops(data)
+check_time(data)
---
+++ Problems/Sort the flowers/main.py
@@ -0,0 +1,7 @@
+# the following line creates a list from the input, do not modify it, please
+prices = [float(price) for price in input().split()]
+
+# your code below
+prices.sort()
+prices.reverse()
+print(prices)
